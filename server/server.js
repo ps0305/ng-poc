6101d27c912a9ef49e3cb92b530a95e7981e3cc2
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-06-01 16:26:35 +0530

    server.js

---
+++ server/server.js
@@ -0,0 +1,7 @@
+//importing modules
+var express = require("express");
+var myqsl = reqire("mysql");
+var mongodb = require("mongodb");
+var jwt = require("jwt-simple");
+var bodyparser = require("body-parser");
+var fs = require("fs");
