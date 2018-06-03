38339f8a47706c4496fe6b45d636fef00118c88d
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-06-03 16:36:47 +0530

    update

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
-import {Http,Response} from "@angular/http";
+import { Http,Response} from "@angular/http";
 import "rxjs/Rx";
 import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class DashboardService {
 
-  constructor(private _service:DashboardService) { }
+  constructor(private _http:Http) { }
 
   public aboutData(){
     return this._http.post("http://localhost:8080/about",{
-      'token':JSON.parse(window.localStorage.getItem("login_details").token)
+      'token':JSON.parse(window.localStorage.getItem("login_details")).token
     }).map((res:Response)=>{
       return res.json();
     }).catch(this._handleError);
@@ -19,7 +19,7 @@ export class DashboardService {
 
 public portfolioData(){
   return this._http.post("http://localhost:8080/portfolio",{
-    'token':JSON.parse(window.localStorage.getItem("login_details").token)
+    'token':JSON.parse(window.localStorage.getItem("login_details")).token
   }).map((res:Response)=>{
     return res.json();
   }).catch(this._handleError);
@@ -28,7 +28,7 @@ public portfolioData(){
 
 public contactData(){
   return this._http.post("http://localhost:8080/contact",{
-    'token':JSON.parse(window.localStorage.getItem("login_details").token)
+    'token':JSON.parse(window.localStorage.getItem("login_details")).token
   }).map((res:Response)=>{
     return res.json();
   }).catch(this._handleError);
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { http ,Response} from '@angular/http'; //http=>to hit the server,Response=>response will be of Response type
+import { Http ,Response } from '@angular/http'; //http=>to hit the server,Response=>response will be of Response type
 import 'rxjs/Rx'; //map,catch and throw function
 import { Observable } from 'rxjs/observable';//Observable=> to throw the error
 
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs/observable';//Observable=> to throw the error
 export class LoginService {
 
 
-  constructor(private _service:LoginService) { }
+  constructor(private _http:Http) { }
   public authenticate(obj):any{
     return this._http.post("http://localhost:8080/login",obj)
     .map((res:Response)=>{
