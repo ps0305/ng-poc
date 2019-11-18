84b177126898614835a86b14fb496fd56f2dc4de
Pankaj Singh <singh.pankaj@live.com>
2019-11-18 20:37:20 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 //importing modules
-var express = require("express");
-var mysql = require("mysql");
-var mongodb = require("mongodb");
-var jwt = require("jwt-simple");
-var bodyparser = require("body-parser");
-var fs = require("fs");
+const express = require("express");
+const mysql = require("mysql");
+const mongodb = require("mongodb");
+const jwt = require("jwt-simple");
+const bodyparser = require("body-parser");
+const fs = require("fs");
 
 //create the RESt object or app instance
-var app = express();
+const app = express();
 
-var router = express.Router();
+const router = express.Router();
 
 //Create mongo client
-var miniProject = mongodb.MongoClient;
+const miniProject = mongodb.MongoClient;
 
 //enable the CORS
 app.use(function(req, res, next) {
@@ -28,28 +28,34 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
 
 //create the connection object
-var connection = mysql.createConnection({host:"localhost",
-                                        user:"root",
-                                        password:"root",
-                                        database:"pocangular2"});
+let connection = mysql.createConnection(
+  {
+    host:"localhost",
+   
+    user:"root",
+   
+    password:"root",
+   
+    database:"pocangular2"
+  });
 //Connect to Database
 connection.connect();
 //create the array to store the generated token(in rea time we store token in databse)
-var tokenArr = [];
+let tokenArr = [];
 
 //Create the "/login" RESt API
 app.post("/login",function(req,res){
-    var uname = req.body.uname;
-    var upwd = req.body.upwd;
+    let uname = req.body.uname;
+    let upwd = req.body.upwd;
     //match client uname n upwd with database credentials
-    connection.query("select * from login_details where uname = '"+uname+"'and upwd = '"+upwd+"'",
-    function(err,recordsArray,fields){
-        if(recordsArray.length>0){
-            var token = jwt.encode({'uname':uname,'upwd':upwd},'hr@nodejs.in')
+    connection.query("select * from login_details where uname = '" + uname + "'and upwd = '" + upwd + "'",
+    function(err, recordsArray, fields){
+        if (recordsArray.length>0) {
+            var token = jwt.encode({'uname':uname, 'upwd':upwd},'hr@nodejs.in')
             tokenArr.push(token);
-            res.send({login:'success',token:token});
-        }else{
-                res.send({login:"failed"});
+            res.send({ login:'success', token:token });
+        } else  {
+                res.send({ login:"failed" });
         }
     });
 });
@@ -58,29 +64,29 @@ app.post("/login",function(req,res){
 
 //about RESt API
 app.post("/about",function(req,res){
-    var token = req.body.token;
-    if(tokenArr[0] == token){
+    let token = req.body.token;
+    if (tokenArr[0] == token) {
         connection.query("select * from products ",
-        function(err,recordsArray,fields){
+        function(err, recordsArray, fields){
                 res.send(recordsArray)
             });
-                   }else{
-                    res.send("Unauthorised Access");
-                }
-            });
+               } else {
+                res.send("Unauthorised Access");
+             }
+           });
 
 //********************************************************** */
 
 //Portfolio RESt API
-app.post("/portfolio",function(req,res){
-    var token = req.body.token;
-    if(tokenArr[0] == token){
-        miniProject.connect("mongodb://localhost:27017/pocAngular2",function(err,db){
-            db.collection("products").find().toArray(function(err,array){
+app.post("/portfolio", function(req, res){
+    let token = req.body.token;
+    if (tokenArr[0] == token) {
+        miniProject.connect("mongodb://localhost:27017/pocAngular2", function(err, db){
+            db.collection("products").find().toArray(function(err, array){
                 res.send(array);
             });
         });
-    }else{
+    } else {
         res.send("Unauthorised access...!");
     }
 });
@@ -88,13 +94,13 @@ app.post("/portfolio",function(req,res){
 //******************************************************************** */
 
 //Contact RESt API
-app.post("/contact",function(req,res){
-    var token = req.body.token;
-    if(tokenArr[0] == token){
-        fs.readFile("../static/contact.json",function(err,data){
+app.post("/contact", function(req, res){
+    let token = req.body.token;
+    if (tokenArr[0] == token) {
+        fs.readFile("../static/contact.json",function(err, data){
             res.send(data);
         });
-    }else{
+    } else {
         res.send("Unauthorised user");
     }
 });
